518bb25d9b6459c7b352dc2db4d4336804656b83
jdelobel5987 <delobel.julien@outlook.com>
2024-12-20 17:45:26 +0100

    first commit

---
+++ assets/js/header-iconebar-footer.js
@@ -0,0 +1,48 @@
+const main = document.querySelector("main");
+// const body = document.querySelector("body");
+
+main.insertAdjacentHTML(
+  "beforebegin",
+  `<header>
+        <div class="container-header">
+            <div class="name"><p>Le<br>Grenier<br>du Joueur</p></div>
+            <div class="container-searchbar">    
+                <div class="searchBar">
+                    <!-- burger menu -->
+                    <input class="input" type="text" placeholder="Rechercher un jeu">
+                    <!-- icone de recherche -->
+                </div>
+            </div>
+        </div>
+    </header>`
+);
+
+main.insertAdjacentHTML(
+  "beforeend",
+  `<div class="icon-bar">
+        <div class="container-icon-bar">
+            <a href="index.html" class="icon" id="icon-search"><i class="fa-solid fa-house fa-2x"></i></a>
+            <a href="products-search.html" class="icon" id="icon-search"><i class="fa-solid fa-magnifying-glass fa-2x"></i></a>
+            <a href="user-account.html" class="icon" id="icon-account"><i class="fa-solid fa-user fa-2x"></i></a>
+            <a href="basket.html" class="icon" id="icon-cart"><i class="fa-solid fa-cart-shopping fa-2x"></i></a>
+            <a href="irl-store.html" class="icon" id="icon-irl-store"><i class="fa-solid fa-dungeon fa-2x"></i></a>
+        </div>
+    </div>`
+);
+
+main.insertAdjacentHTML(
+  "afterend",
+  `<footer>
+        <div class="container-footer">
+            <p>Copyright © Julien Delobel 2024</p> 
+            <p>site fictif d'e-commerce: Le Grenier du Joueur</p>
+            <a href="./assets/pdf/Mentions_Légales_Le_Grenier_du_Joueur.pdf">Mentions légales</a>
+            <a href="./assets/pdf/Politique_de_Confidentialite_Le_Grenier_du_Joueur.pdf">Politiques de confidentialité</a>
+        </div>
+    </footer>`
+);
+
+// body.insertAdjacentHTML(
+//     "beforeend",
+//     // `js scripts here`
+// )
---
+++ assets/js/index_resize-carousel.js
@@ -0,0 +1,104 @@
+/////////////////////////////////////////////////////////////////////////////////////
+///// set carousel image width class (w-100 w-75 w-75) according to screen size /////
+/////////////////////////////////////////////////////////////////////////////////////
+
+let thresholdMobile = 500; // 320; 375; 425; 768; 1024; 1440;
+let thresholdDesktop = 1000; // 320; 375; 425; 768; 1024; 1440;
+
+// select all images within the carousel
+const carImg = document.querySelectorAll(".carousel-inner img");
+
+// set img width based on current screen size (will do on page load)
+let screenWidth = window.innerWidth;
+
+for (let i = 0; i < carImg.length; i++) {
+  if (screenWidth <= thresholdMobile) {
+    carImg[i].classList.add("w-75");
+  } else if (screenWidth <= thresholdDesktop) {
+    carImg[i].classList.add("w-50");
+  } else {
+    carImg[i].classList.add("w-25");
+  }
+
+  value =
+    screenWidth <= thresholdMobile
+      ? "carousel img width set to w-75"
+      : screenWidth <= thresholdDesktop
+      ? "carousel img width set to w-50"
+      : "carousel img width set to w-25";
+  console.log(`screen width: ${screenWidth} px
+    ${value}`);
+}
+
+// create a function to create an array of values between initial and resized screenWidth values
+function createRange(x, y) {
+  let len = Math.abs(x - y) + 1;
+  let arr = new Array(len);
+  for (let i = 0; i < len; i++) {
+    arr[i] = Math.min(x, y) + i;
+  }
+  return arr;
+}
+
+// create a function to change the width of images only if screen size change around the selected thresholds
+function handleResize() {
+  let newScreenWidth = window.innerWidth;
+  console.log(`previous screen width: ${screenWidth} px`);
+  console.log(`current screen width: ${newScreenWidth} px`);
+
+  // call the array function with actual values
+  let deltaWidth = createRange(screenWidth, newScreenWidth);
+
+  // algorith for carousel images width class change
+  if (deltaWidth.includes(thresholdMobile || thresholdDesktop)) {
+    for (let i = 0; i < carImg.length; i++) {
+      if (screenWidth <= thresholdMobile) {
+        carImg[i].classList.remove("w-75");
+        if (newScreenWidth <= thresholdDesktop) {
+          carImg[i].classList.add("w-50");
+        } else {
+          carImg[i].classList.add("w-25");
+        }
+      } else if (screenWidth <= thresholdDesktop) {
+        carImg[i].classList.remove("w-50");
+        if (newScreenWidth <= thresholdMobile) {
+          carImg[i].classList.add("w-75");
+        } else {
+          carImg[i].classList.add("w-25");
+        }
+      } else {
+        carImg[i].classList.remove("w-25");
+        if (newScreenWidth <= thresholdMobile) {
+          carImg[i].classList.add("w-75");
+        } else {
+          carImg[i].classList.add("w-50");
+        }
+      }
+    }
+  }
+
+  // update the screen width variable for next resize event check
+  screenWidth = newScreenWidth;
+}
+
+// apply the function when the screen is resized
+window.addEventListener("resize", handleResize);
+
+// handleResize();
+
+///////////////////////////////////////////////////////////////////////////////
+
+// // 1st version of algorithm for carousel images width class change
+// if (screenWidth <= thresholdMobile && newScreenWidth > thresholdMobile) {
+//     carImg[i].classList.remove("w-75");
+//     if (newScreenWidth <= thresholdDesktop) {
+//       carImg[i].classList.add("w-50");
+//     } else {
+//       carImg[i].classList.add("w-25");
+//     }
+//     console.log("carousel img width set to w-100");
+//   } else if (screenWidth > threshold && newScreenWidth <= threshold) {
+//     carImg[i].classList.add("w-75");
+//     carImg[i].classList.remove("w-100");
+//     console.log("carousel img width set to w-75");
+//   }
