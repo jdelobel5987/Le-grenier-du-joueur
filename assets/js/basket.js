2a05c6f35b363585bc1205bc411e7232928affed
jdelobel5987 <delobel.julien@outlook.com>
2024-12-30 17:48:16 +0100

    page panier: ajout 1) update prix panier au retrait d'articles, 2) update du bouton de validation si aucun article sélectionné, 3) gestion panier vide (au chargement ou par suppression du dernier article du panier)

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,13 +1,45 @@
+const container = document.querySelector(".container");
+const basketContainer = document.querySelector(".basket-container");
 const basket = document.querySelectorAll(".basket-unit");
 const prices = document.querySelectorAll(".basket-unit span");
-console.log(prices);
 
 let totalPrice = 0;
-if (prices.length != 0) {
+
+function empty() {
+  container.innerHTML = "";
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="empty-basket">
+        <h2>Votre panier est vide <i class="fa-solid fa-face-sad-tear"></i></h2><br>
+        <p><i class="fa-solid fa-magnifying-glass fa-2x"></i>  Explorez nos merveilleux produits </p>
+        <p>&nbsp&nbsp&nbsp ou venez nous-rendre visite&nbsp&nbsp<i class="fa-solid fa-dungeon fa-2x"></i></p>
+    </div>`
+  );
+}
+
+function buttonUpdate() {
+  const button = document.querySelector(".btn");
+  if (totalPrice == 0) {
+    button.disabled = true;
+    button.style.opacity = 0.5;
+    button.textContent = "Aucun article sélectionné";
+  } else {
+    button.disabled = false;
+    button.style.opacity = 1;
+    button.textContent = "Valider mon panier";
+  }
+}
+
+if (!basketContainer.children.length) {
+  empty();
+}
+
+if (prices.lenght != 0) {
   prices.forEach((price) => {
     totalPrice += parseFloat(price.textContent);
   });
-  document.querySelector("#total").textContent = `${totalPrice} €`;
+  document.querySelector("#total").textContent = `Total : ${totalPrice} €`;
+  buttonUpdate();
 }
 
 basket.forEach((basketUnit) => {
@@ -15,29 +47,42 @@ basket.forEach((basketUnit) => {
   const value = basketUnit.querySelector("span");
   const price = parseFloat(value.textContent);
   const remove = basketUnit.querySelector("i");
+  const product = basketUnit.querySelector("label").textContent;
 
   checkbox.addEventListener("change", () => {
     if (checkbox.checked != true) {
       basketUnit.style.opacity = 0.5;
       totalPrice -= price;
       document.querySelector("#total").textContent = `${totalPrice} €`;
-      console.log("article déselectionné, total H.T. mis à jour");
+      console.log(`${product} déselectionné, montant total mis à jour`);
+      buttonUpdate();
     } else {
       basketUnit.style.opacity = 1;
       totalPrice += price;
       document.querySelector("#total").textContent = `${totalPrice} €`;
-      console.log("article sélectionné, total H.T. mis à jour");
+      console.log(`${product} sélectionné, montant total mis à jour`);
+      buttonUpdate();
     }
   });
 
   remove.addEventListener("click", () => {
     let confirmSupp = confirm(
-      "Êtes-vous sur de vouloir supprimer cet article ?"
+      `Êtes-vous sur de vouloir supprimer ${product} de votre panier?`
     );
     if (confirmSupp) {
       basketUnit.parentNode.removeChild(basketUnit);
-      alert("L'article a bien été supprimé");
-      console.log("suppression d'un article du panier");
+      if (checkbox.checked == true) {
+        totalPrice -= price;
+        document.querySelector("#total").textContent = `${totalPrice} €`;
+      }
+      alert(`${product} a bien été supprimé du panier`);
+      console.log(
+        `suppression de ${product} du panier validée par l'utilisateur.
+        Montant total mis à jour: ${totalPrice} €`
+      );
+    }
+    if (!basketContainer.children.length) {
+      empty();
     }
   });
 });
