3242e64deb913475a7f5e1d3a726132bdbd58ba0
jdelobel5987 <delobel.julien@outlook.com>
2025-01-12 17:04:10 +0100

    add icon bar dynamics according to displayed page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
+// validate searchbar input by clicking on the search icon or pressing enter
 const search = document.querySelector(".searchBar>input");
 const searchBtn = document.querySelector(".searchBar>button");
+let input = "";
 
 document.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
-    let input = search.value;
+    input = search.value;
     console.log(input);
   }
 });
 
 searchBtn.addEventListener("click", () => {
-  let input = search.value;
+  input = search.value;
   console.log(input);
 });
 
@@ -41,3 +43,36 @@ if (userData) {
   })
   iconBarUser.href = "connection.html"
 }
+
+// modify iconbar display of the icon for current page
+const icons = document.querySelectorAll('.icon-bar .fa-solid');
+console.log(icons);
+const pageLinks = document.querySelectorAll('.icon-bar a');
+console.log(pageLinks);
+
+// icons.forEach((icon) => {
+//   icon.addEventListener('click', () => {
+//     // icon.classList.replace('fa-2x', 'fa-3x');
+//     icon.style.color = 'black';
+//     icon.parentElement.style.border = '2px solid black';
+//     icon.parentElement.style.borderRadius = '30%';
+//   })
+// });
+
+pageLinks.forEach((link) => {
+  if (document.location.pathname === link.pathname) {
+    let icon = link.querySelector('i');
+    // icon.setAttribute('data-fa-transform', 'grow-3 up-6');
+    icon.style.color = 'black';
+    icon.style.height = '70px';
+    // // icon.style.width = '70px';
+    icon.style.backgroundColor = 'var(--myColYellow)';
+    // // icon.style.border = '1px solid var(--myColBrown)';
+    icon.style.borderRadius = '50%';
+    icon.classList.replace('fa-2x', 'fa-3x');
+    // // icon.style.position = 'absolute';
+    // // icon.style.top = 0;
+    // // // icon.style.left = calc(((indexOf(icon) + 1) * 20) - 10) + '%';
+    // // icon.style.transform = 'translate(-40%, 0%)';
+  }
+});
