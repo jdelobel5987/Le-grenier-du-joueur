567e62377eee5f7f6334a303dcb29bfeb534205f
jdelobel5987 <delobel.julien@outlook.com>
2025-04-27 21:44:19 +0200

    update error404 view and css

--- src/views/error404.php
+++ src/views/error404.php
@@ -1,57 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="page non trouvée, supprimée ou inexistante">
     <title>Page non trouvée</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f9f9f9;
-            text-align: center;
-            padding: 50px;
-        }
-        h1 {
-            font-size: 50px;
-            color: #333;
-        }
-        p {
-            font-size: 20px;
-            color: #666;
-        }
-        .container {
-            background-color: white;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            display: inline-block;
-        }
-        .game-icon {
-            width: 100px;
-            height: 100px;
-        }
-        .btn {
-            display: inline-block;
-            margin-top: 20px;
-            padding: 10px 20px;
-            font-size: 18px;
-            color: white;
-            background-color: #ff6347;
-            text-decoration: none;
-            border-radius: 5px;
-        }
-        .btn:hover {
-            background-color: #ff4500;
-        }
-    </style>
+    <link rel="stylesheet" href="/assets/css/error404.css">
 </head>
+
 <body>
     <div class="container">
-        <!-- <img src="path/to/game-icon.png" alt="Jeu de société" class="game-icon"> -->
-        <h1>404 - Page non trouvée</h1>
+        <img src="assets/img/icones/meeple.svg" alt="icone de meeple inversé" class="meeple-icon">
+        <h1>erreur 404 - Page non trouvée</h1>
         <p>Oups ! Il semble que vous ayez perdu votre chemin dans notre ludothèque.</p>
         <p>Retournez à l'accueil pour continuer à explorer nos jeux de société passionnants.</p>
         <a href="/" class="btn">Retour à l'accueil</a>
     </div>
 </body>
+
 </html>
